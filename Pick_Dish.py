1acd075c7fc02afff3c4219d2d318096c21ff409
Joycelyn <jwchen.tw@gmail.com>
2020-02-10 17:50:13 +0800

    Add some testing data

---
+++ Pick_Dish.py
@@ -0,0 +1,25 @@
+from DataStructure import tags_map, Dish, Example
+
+
+#creating some data for testing
+Apple = Item('apple', 'Joy kitchen', 'fruit')
+Banana = Item('banana', 'Joy kitchen', 'fruit')
+data = [
+		['clam noodle', 'Joy kitchen', 'main02'],
+		['fried rice', 'Joy kitchen', 'main01'],
+		['steam dumpling', 'Joy kitchen', 'main03']
+		]
+
+
+
+listA = ['A', 'B', 'C']
+for idx in range(len(listA)):
+	if hasTag()
+    print(listA[idx])     # A B C
+
+
+
+theExample = Example()
+print(theExample.itsProblem)
+print (Example.itsProblem)
+
